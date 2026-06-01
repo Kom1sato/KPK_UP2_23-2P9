93e77ba9d0591677c195d7e12ea284a1935b239c
Kom1sato <kokarevmix111057@gmail.com>
2026-06-01 16:50:21 +0300

    Create models.py

---
+++ S16/models.py
@@ -0,0 +1,26 @@
+from peewee import *
+
+db = SqliteDatabase("campus.db")
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Building(BaseModel):
+    name = CharField(max_length=100)
+    address = CharField(max_length=255)
+    floors = IntegerField()
+    is_active = BooleanField(default=True)
+
+    class Meta:
+        indexes = (
+            (("name", "address"), True),
+        ) 
+
+def initialize_database():
+    db.connect()
+    db.create_tables([Building], safe=True)
+    db.close()
+
+if __name__ == "__main__":
+    initialize_database()

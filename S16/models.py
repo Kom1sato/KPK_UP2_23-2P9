d5ccc3721a79887eadded21eaf2fc40860c0b62d
Kom1sato <kokarevmix111057@gmail.com>
2026-06-03 12:55:50 +0300

    Update models.py

--- S16/models.py
+++ S16/models.py
@@ -7,20 +7,20 @@ class BaseModel(Model):
         database = db
 
 class Building(BaseModel):
-    name = CharField(max_length=100)
-    address = CharField(max_length=255)
-    floors = IntegerField(constraints=[Check("floors > 0")])
-    is_active = BooleanField(default=True)
+    name = CharField(max_length=100, null=False)
+    address = CharField(max_length=255, null=False)
+    floors = IntegerField(constraints=[Check("floors > 0")], null=False)
+    is_active = BooleanField(default=True, null=False)
 
     class Meta:
         indexes = (
             (("name", "address"), True),
         ) 
 
-def initialize_database():
+def init_db():
     db.connect()
     db.create_tables([Building], safe=True)
     db.close()
 
 if __name__ == "__main__":
-    initialize_database()
+    init_db()
